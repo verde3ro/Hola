613c22c6e3e6b6ab5ad47b0f797f7223be4a6244
verde3ro <verde3ro@gmail.com>
2021-09-04 11:42:31 -0500

    Se crearon matrcices en java

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		// System.out.println("Hello, World!");
 		for (int i = 0; i < args.length; i++) {
-			System.out.println(i + " :: " + args[i]);
+			System.out.println(i + " :: " + args[i] + "");
 		}
 	}
 }
---
+++ src/Arreglo.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class Arreglo {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 9, 10 };
+		String[] nombres = { "Raúl", "Alejandro", "Verde", "Martínez" };
+
+		for (int valor : arr) {
+			System.out.println(String.format("Valor %d", valor));
+		}
+
+		for (String valor : nombres) {
+			System.out.println(String.format("Valor %s", valor));
+		}
+
+		// Redimencionar arreglos
+		String[] nombres2 = Arrays.copyOf(nombres, nombres.length + 2);
+		nombres2[nombres.length] = "verde3ro";
+		nombres2[nombres.length + 1] = "green";
+
+		for (String valor : nombres2) {
+			System.out.println(String.format("Valor %s", valor));
+		}
+
+		// Matrices
+		// int matriz1[] [] = new int[] []{}
+		// int matriz2[] [] = new int[3] [3];
+		int matriz3[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		for (int i = 0; i < matriz3.length; i++) {
+			for (int j = 0; j < matriz3[i].length; j++) {
+				System.out.println(String.format("Valor %d", matriz3[i][j]));
+			}
+		}
+
+		System.out.println("*** Jagged Array **");
+		// Matriz iregular o dentada
+		int matriz4[][] = { { 3, 4, 5 }, { 77, 50 } };
+
+		for (int[] elemento : matriz4) {
+			for (int valor : elemento) {
+				System.out.println(String.format("Valor %d", valor));
+			}
+		}
+
+		System.out.println("*** Object Array **");
+		Object[] c = { "Hola", 's', 40L };
+		var y = 21;
+		for (Object elemento : c) {
+			System.out.println("Valor ::" + elemento);
+		}
+	}
+}
